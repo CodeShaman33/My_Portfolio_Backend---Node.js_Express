9f2578b18d540e9047368b89680e5d74f91a03ef
PsychoShaman <111572177+PsychoShaman@users.noreply.github.com>
2023-02-21 11:21:43 +0100

    add functions that handle requests

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 //app
 const app = express();
+
+app.get('/', (req, res) =>
+{
+    res.send('Hello World');
+});
+
+app.post('/todos', (req, res) =>
+{
+    console.log('object got');
+    res.status(200).end();
+    
+    
+}
+)
+
+
 app.listen(8888, () =>
  {
     console.log('app has started on port 8888');
